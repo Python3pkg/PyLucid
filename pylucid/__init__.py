70c9c022add34a8053966bf98892d212c8c4454a
JensDiemer <git@jensdiemer.de>
2017-02-08 10:19:10 +0100

    Update packages, e.g.: django-cms v3.4.x, ckeditor v3.4.x

--- pylucid/__init__.py
+++ pylucid/__init__.py
@@ -3,8 +3,8 @@
     PyLucid
     ~~~~~~~
 
-    :copyleft: 2009-2016 by the PyLucid team, see AUTHORS for more details.
+    :copyleft: 2009-2017 by the PyLucid team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
-__version__ = "2.1.2"
+__version__ = "2.2.0"
