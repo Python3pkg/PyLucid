8ccf8eb435a19bb08678f8c7325ecc3141717546
JensDiemer <git@jensdiemer.de>
2017-02-08 10:14:10 +0100

    server static if "runserver" used

--- pylucid/base_urls.py
+++ pylucid/base_urls.py
@@ -1,9 +1,21 @@
-from cms.sitemaps import CMSSitemap
+# coding: utf-8
+"""
+    PyLucid
+    ~~~~~~~
+
+    :copyleft: 2009-2017 by the PyLucid team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+import sys
+
 from django.conf.urls import url, include, patterns
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.conf import settings
+
+from cms.sitemaps import CMSSitemap
 
 
 admin.autodiscover()
@@ -22,7 +34,7 @@ urlpatterns = i18n_patterns('',
     url(r'^', include('cms.urls')),
 )
 
-# This is only needed when using runserver.
+
 if settings.DEBUG:
     if 'debug_toolbar' in settings.INSTALLED_APPS:
         import debug_toolbar
@@ -30,6 +42,10 @@ if settings.DEBUG:
             url(r'^__debug__/', include(debug_toolbar.urls)),
         ) + urlpatterns
 
+
+if "runserver" in sys.argv:
+    # This is only needed when using runserver.
+    print("Activate static file serving...")
     urlpatterns = patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve',  # NOQA
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
